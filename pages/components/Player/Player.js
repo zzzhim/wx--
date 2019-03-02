b061d800ccb5b965ee11a7b6442bf5c77cbc3614
wangyafei <3218566389@qq.com>
2019-03-03 00:15:21 +0800

    解决音乐播放切换页面时的已知问题

--- pages/components/Player/Player.js
+++ pages/components/Player/Player.js
@@ -1,6 +1,8 @@
 Component({
     properties: {
-        movieData: Object
+        movieData: {
+            type: Object,
+        }
     },
 
     data: {
@@ -10,48 +12,93 @@ Component({
     },
 
     ready() {
+        const music = wx.getStorageSync('music')
+
         this.data.music = wx.getBackgroundAudioManager()
-        this.data.music.title = this.properties.movieData.title
+
+        if(music.index == this.data.movieData.index) {
+            const play = wx.getStorageSync('play')
+
+            if (play.play) {
+                this.data.music.src = this.properties.movieData.url
+                this.data.music.title = this.properties.movieData.title
+
+                this.data.music.startTime = music.time
+            }
+
+            this.setData({ play: play.play })
+        }
 
         // 监听背景音频播放事件
         this.data.music.onPlay(() => {
-            console.log(this.data.music.title)
-            this.setData({ play: true })
+            this.setData({ play: true, bool: false })
         })
 
         // 监听音乐停止
         this.data.music.onStop(() => {
-            this.setData({ play: false })
+            this.setData({ bool: true,play: false })
         })
 
 
         // 监听背景音频暂停事件
         this.data.music.onPause(() => {
             this.setData({ play: false })
+
+            const obj = wx.getStorageSync('play')
+            obj.play = false
+            wx.setStorage({
+                key: 'play',
+                data: obj
+            })
         })
 
         // 监听背景音频自然播放结束事件
         this.data.music.onEnded(() => {
-            this.setData({ play: false })
+            this.setData({ bool: true, play: false })
         })
 
         // 监听背景音频播放错误事件
         this.data.music.onError(() => {
-            this.setData({ play: false })
+            this.setData({ bool: true, play: false })
         })
+        
+        // 监听背景音频播放进度更新事件
+        this.data.music.onTimeUpdate(() => {
+            const play = wx.getStorageSync('play')
+
+            const obj = {
+                index: play.index,
+                time: this.data.music.currentTime
+            }
+
+            wx.setStorage({
+                key: 'music',
+                data: obj
+            })
+        })
+
     },
     
     methods: {
         onPlay() {
             if(this.data.bool) {
+                this.data.music.title = this.properties.movieData.title
                 this.data.music.src = this.properties.movieData.url
-                this.setData({ bool: false })
-            }
 
-            this.data.music.play()
+                wx.setStorage({
+                    key: 'play',
+                    data: {
+                        index: this.data.movieData.index,
+                        play: true
+                    }
+                })
+
+                this.data.music.play()
+            }else {
+                this.data.music.play()
+            }
         },
         onOut() {
-            console.log('stop')
             this.data.music.pause()
         }
     }
