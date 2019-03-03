99d94f0116db08420e73b32e90c75f7ee7a36f3d
wangyafei <3218566389@qq.com>
2019-03-03 18:01:19 +0800

    获取热门书籍数据，渲染到页面

--- pages/components/Book/Book.js
+++ pages/components/Book/Book.js
@@ -1,66 +1,27 @@
-// pages/components/Book/Book.js
-Page({
+import $request from "../../../utils/request.js"
 
-    /**
-     * 页面的初始数据
-     */
+Component({
+    properties: {
+        book: Object
+    },
+
     data: {
-
+        comment: 0
     },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
+    ready() {
+        this.GetComment()
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
+    methods: {
+        GetComment() {
+            $request(`/book/${this.properties.book.id }/short_comment`)
+                .then(res => {
+                    console.log(res)
+                    this.setData({ comment: res.comments.length })
+                })
+        }
     },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
+    
 })
