6409e380fb13f5366ebecdc5d02ba073100937ca
zzzhim <3218566389@qq.com>
2019-05-15 17:22:37 +0800

    喜欢 底部组件页面完成

--- pages/like/components/header/header.js
+++ pages/like/components/header/header.js
@@ -1,3 +1,17 @@
-Component({
+import $request from "../../../../utils/request.js"
 
+Component({
+    data: {
+        count: 0
+    },
+
+    ready() {
+        $request('/book/favor/count')
+            .then(res => {
+                this.setData({ count: res.count })
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
 })
